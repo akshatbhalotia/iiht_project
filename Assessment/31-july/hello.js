a5d7163897870ffe35347b383d673c8ecfb8e51d
akshatbhalotia <32238556+akshatbhalotia@users.noreply.github.com>
2019-07-31 16:28:53 +0530

    Add files via upload

---
+++ Assessment/31-july/hello.js
@@ -0,0 +1,24 @@
+function sayHello() {
+ alert("Hello World, this is from js file");
+}
+
+function add(a, b)
+{
+var c;
+c = a + b;
+console.log('Can u see this?');
+alert('The result is '+c);
+
+}
+
+
+
+
+
+function myFunction(x) {
+  x.style.background = "yellow";
+}
+
+function myFunction1(y) {
+  y.style.background = "white";
+}
